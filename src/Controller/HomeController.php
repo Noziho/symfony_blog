92d8fb5a927d8422de9949d78b9f99890eaa3dc3
Noziho <noah.decroix3@gmail.com>
2023-03-03 14:01:23 +0100

    remove unused use

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Form\CommentaryType;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
