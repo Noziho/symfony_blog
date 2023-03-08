419c19613d0bb52b995e81f33791e4bda0d7939e
Noziho <noah.decroix3@gmail.com>
2023-03-08 15:48:03 +0100

    start translation

---
+++ translations/messages.fr.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'Welcome' => 'Bonjour',
+    'Logout' => 'Déconnexion',
+    'Profile' => 'Profil',
+    'Register' => 'S\'inscrire',
+    'Login' => 'Connexion',
+    'Show' => 'Voir',
+    'Your profile' => 'Votre profil',
+    'Your articles' => 'VOs articles',
+    'Publish status' => 'Publiez l\'article',
+    'Add an new article' => 'Ajoutez un article',
+    'No article yet' => 'Il n\'y a pas encore d\'article publié',
+    'Edit' => 'Modifier',
+    'Commentaries' => 'Commentaires',
+    'Delete' => 'Supprimer',
+    'Add comment' => 'Ajouter un commentaire',
+    'Password' => 'Mot de passe',
+    'Agree terms' => 'Accepter les termes',
+    'Please sign in' => 'Se connecter',
+    'Create new Article' => 'Créer un nouvelle article',
+    'Create new Commentary' => 'Ajouter un commentaire',
+];
